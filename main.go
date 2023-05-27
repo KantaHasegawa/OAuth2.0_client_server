eeb90d4579db1bf6de2d928a5c5e176f2cb82d19
KantaaHasegawa <gyoza20000@gmail.com>
2023-05-27 23:22:30 +0900

    add gin

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
-
+import (
+	"github.com/gin-gonic/gin"
+)
 func main() {
-	fmt.Println("Hello Wolrd")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context){
+		c.JSON(200, gin.H{
+			"message": "I am root end point",
+		})
+	})
+	r.Run()
 }
